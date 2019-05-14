4bf3ac42db9f10d5dd98452542039494ea21b51e
jardinehc <38780007+jardinehc@users.noreply.github.com>
2019-05-14 12:47:42 +1200

    Update api.specs.js
    testing my testing

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -89,7 +89,7 @@
           	} else {
               assert.equal(resp.statusCode, 200);
               var pbody = JSON.parse(body);
-              assert(pbody.city === 'London', "City name does not match");
+              assert(pbody.city === 'London', "City name does not match, result gives: " + pbody.city);
               done();
             }
         });
